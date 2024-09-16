6a3baae746055aeac216a70815261f2aca0d9c20
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-16 14:58:17 +0200

    finished core exercise.

--- src/main/java/com/booleanuk/api/security/SecurityConfiguration.java
+++ src/main/java/com/booleanuk/api/security/SecurityConfiguration.java
@@ -1,11 +1,17 @@
 package com.booleanuk.api.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfiguration {
 
     @Bean
@@ -15,5 +21,15 @@ public class SecurityConfiguration {
                 .formLogin(form -> form.loginPage("/login").permitAll())
                 .logout(LogoutConfigurer::permitAll);
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
